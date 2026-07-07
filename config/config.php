947dc907fc44127ddab21a8a72f26ed45f625cbc
jokiller220 <joakimazomedon@gmail.com>
2026-07-07 15:29:48 +0200

    Robust env variable parsing

--- config/config.php
+++ config/config.php
@@ -1,10 +1,20 @@
 <?php
 // config.php
-define('DB_HOST', getenv('MYSQLHOST') ?: 'localhost');
-define('DB_USER', getenv('MYSQLUSER') ?: 'root');
-define('DB_PASS', getenv('MYSQLPASSWORD') ?: 'root'); // MAMP default password for root is 'root'
-define('DB_NAME', getenv('MYSQLDATABASE') ?: 'race_elu_db');
-define('DB_PORT', getenv('MYSQLPORT') ?: '3306');
+function get_env_var($keys, $default = '') {
+    foreach ((array)$keys as $key) {
+        $val = getenv($key);
+        if ($val !== false && $val !== '') return $val;
+        if (isset($_ENV[$key]) && $_ENV[$key] !== '') return $_ENV[$key];
+        if (isset($_SERVER[$key]) && $_SERVER[$key] !== '') return $_SERVER[$key];
+    }
+    return $default;
+}
+
+define('DB_HOST', get_env_var(['MYSQLHOST', 'MYSQL_HOST'], 'localhost'));
+define('DB_USER', get_env_var(['MYSQLUSER', 'MYSQL_USER'], 'root'));
+define('DB_PASS', get_env_var(['MYSQLPASSWORD', 'MYSQL_PASSWORD'], 'root')); // MAMP default password for root is 'root'
+define('DB_NAME', get_env_var(['MYSQLDATABASE', 'MYSQL_DATABASE'], 'race_elu_db'));
+define('DB_PORT', get_env_var(['MYSQLPORT', 'MYSQL_PORT'], '3306'));
 
 // Base URL de l'application dynamique (gère le port automatiquement)
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
